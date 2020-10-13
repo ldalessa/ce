aca7ea20033f67bf3092a1a9fdfd3ebfde5c2473
Luke D'Alessandro <ldalessa@uw.edu>
2020-10-13 09:46:27 -0700

    Restructure the tests a bit.
    Catch2 makes debugging constexpr unit tests quite cumbersome. This exports the
    test lambdas into their own functions to make the stack traces a bit easier to
    digest.

--- tests/cvector.cpp
+++ tests/cvector.cpp
@@ -82,7 +82,6 @@ struct Foo {
 
   constexpr Foo& operator=(Foo&& rhs) {
     a = rhs.a;
-    //assert(rhs.b == 0);
     rhs.b = -2;
     if (!std::is_constant_evaluated()) {
       fmt::print(" Foo = Foo&&({}) (move)\n", a);
@@ -104,68 +103,79 @@ struct Foo {
   }
 };
 
+template <typename T>
+constexpr bool basic_ctor() {
+  cvector<T, 3> a;
+  assert(size(a) == 0);
+  cvector<T, 3> b(3);
+  assert(size(b) == 3);
+  return true;
+}
+static_assert(basic_ctor<Foo>());
+
 TEMPLATE_TEST_CASE("Basic Construction", "[cvector][ctor]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a;
-    assert(size(a) == 0);
-    cvector<TestType, 3> b(3);
-    assert(size(b) == 3);
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(basic_ctor<TestType>());
+  basic_ctor<TestType>();
+}
+
+template <typename T>
+constexpr bool sized_ctor() {
+  cvector<T, 3> a(0);
+  assert(size(a) == 0);
+  cvector<T, 3> b(2);
+  assert(size(b) == 2);
+  cvector<T, 3> c(3);
+  assert(size(c) == 3);
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Sized Construction", "[cvector][ctor]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a(0);
-    assert(size(a) == 0);
-    cvector<TestType, 3> b(2);
-    assert(size(b) == 2);
-    cvector<TestType, 3> c(3);
-    assert(size(c) == 3);
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(sized_ctor<TestType>());
+  sized_ctor<TestType>();
+}
+
+template <typename T>
+constexpr bool read() {
+  cvector<T, 3> a(2);
+  assert(size(a) == 2);
+  assert(a[0] == T());
+  assert(a[1] == T());
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Read Access", "[cvector][read]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a(2);
-    assert(size(a) == 2);
-    assert(a[0] == TestType());
-    assert(a[1] == TestType());
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(read<TestType>());
+  read<TestType>();
 }
 
+template <typename T>
+constexpr bool write() {
+  cvector<T, 3> a(2);
+  assert(size(a) == 2);
+  a[0] = T(1);
+  a[1] = T(2);
+  assert(a[0] == T(1));
+  assert(a[1] == T(2));
+  return true;
+ }
+
 TEMPLATE_TEST_CASE("Write Access", "[cvector][write]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a(2);
-    assert(size(a) == 2);
-    a[0] = TestType(1);
-    a[1] = TestType(2);
-    assert(a[0] == TestType(1));
-    assert(a[1] == TestType(2));
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(write<TestType>());
+  write<TestType>();
+}
+
+template <typename T>
+constexpr bool ctad_inplace() {
+  cvector<T, 3> a = { std::in_place, 1, 2 };
+  assert(size(a) == 2);
+  assert(a[0] == T(1));
+  assert(a[1] == T(2));
+  return true;
 }
 
 TEMPLATE_TEST_CASE("CTAD Variadic Construction", "[cvector][ctad]", int, Foo) {
-  constexpr auto inplace = [] {
-    cvector<TestType, 3> a = { std::in_place, 1, 2 };
-    assert(size(a) == 2);
-    assert(a[0] == TestType(1));
-    assert(a[1] == TestType(2));
-    return true;
-  };
-  static_assert(inplace());
-  inplace();
+  static_assert(ctad_inplace<TestType>());
+  ctad_inplace<TestType>();
 }
 
 TEMPLATE_TEST_CASE("CTAD Variadic Construction with Conversion", "[cvector][ctad]", int) {
@@ -183,261 +193,298 @@ TEMPLATE_TEST_CASE("CTAD Variadic Construction with Conversion", "[cvector][ctad
   assert(c[2] == -1.0);
 }
 
+template <typename T>
+constexpr bool resize() {
+  cvector<T, 3> a;
+  a.resize(1);
+  assert(size(a) == 1);
+  return true;
+}
+
 TEMPLATE_TEST_CASE("Resizing", "[cvector][resize]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a;
-    a.resize(1);
-    assert(size(a) == 1);
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(resize<TestType>());
+  resize<TestType>();
+}
+
+template <typename T>
+constexpr bool resize_data() {
+  cvector<T, 3> a = { std::in_place, 1, 2 };
+  a.resize(2);
+  assert(size(a) == 2);
+  assert(a[1] == T(2));
+  assert(a[0] == T(1));
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Resizing with data", "[cvector][resize]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a = { std::in_place, 1, 2 };
-    a.resize(2);
-    assert(size(a) == 2);
-    assert(a[1] == TestType(2));
-    assert(a[0] == TestType(1));
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(resize_data<TestType>());
+  resize_data<TestType>();
+}
+
+template <typename T>
+constexpr bool resize_smaller() {
+  cvector<T, 3> a = { std::in_place, 1, 2 };
+  a.resize(1);
+  assert(size(a) == 1);
+  assert(a[0] == T(1));
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Resizing smaller", "[cvector][resize]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a = { std::in_place, 1, 2 };
-    a.resize(1);
-    assert(size(a) == 1);
-    assert(a[0] == TestType(1));
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(resize_smaller<TestType>());
+  resize_smaller<TestType>();
+}
+
+template <typename T>
+constexpr bool resize_larger() {
+  cvector<T, 3> a = { std::in_place, 1, 2 };
+  a.resize(3);
+  assert(size(a) == 3);
+  assert(a[2] == T());
+  assert(a[1] == T(2));
+  assert(a[0] == T(1));
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Resizing larger", "[cvector][resize]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a = { std::in_place, 1, 2 };
-    a.resize(3);
-    assert(size(a) == 3);
-    assert(a[2] == TestType());
-    assert(a[1] == TestType(2));
-    assert(a[0] == TestType(1));
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(resize_larger<TestType>());
+  resize_larger<TestType>();
+}
+
+template <typename T>
+constexpr bool pop() {
+  cvector<T, 3> a = { std::in_place, 1, 2};
+  assert(size(a) == 2);
+  assert(a.pop_back() == T(2));
+  assert(a.pop_back() == T(1));
+  assert(size(a) == 0);
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Pop", "[cvector][pop]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a = { std::in_place, 1, 2};
-    assert(size(a) == 2);
-    assert(a.pop_back() == TestType(2));
-    assert(a.pop_back() == TestType(1));
-    assert(size(a) == 0);
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(pop<TestType>());
+  pop<TestType>();
+}
+
+template <typename T>
+constexpr bool emplace() {
+  cvector<T, 3> a;
+  a.emplace_back(1);
+  a.emplace_back(2);
+  assert(size(a) == 2);
+  assert(a[1] == T(2));
+  assert(a[0] == T(1));
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Emplace", "[cvector][emplace]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a;
-    a.emplace_back(1);
-    a.emplace_back(2);
-    assert(size(a) == 2);
-    assert(a[1] == TestType(2));
-    assert(a[0] == TestType(1));
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(emplace<TestType>());
+  emplace<TestType>();
+}
+
+template <typename T>
+constexpr bool push_ref() {
+  cvector<T, 3> a;
+  T one(1), two(2);
+  a.push_back(one);
+  a.push_back(two);
+  assert(size(a) == 2);
+  assert(a[1] == T(2));
+  assert(a[0] == T(1));
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Pushing references", "[cvector][push]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a;
-    TestType one(1), two(2);
-    a.push_back(one);
-    a.push_back(two);
-    assert(size(a) == 2);
-    assert(a[1] == TestType(2));
-    assert(a[0] == TestType(1));
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(push_ref<TestType>());
+  push_ref<TestType>();
 }
 
-TEMPLATE_TEST_CASE("Pushing r-value references", "[cvector][push]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a;
-    TestType one(1), two(2);
-    a.push_back(std::move(one));
-    a.push_back(std::move(two));
-    assert(size(a) == 2);
-    assert(a[1] == TestType(2));
-    assert(a[0] == TestType(1));
-    return true;
-  };
-  static_assert(test());
-  test();
+template <typename T>
+constexpr bool push_rref() {
+  cvector<T, 3> a;
+  T one(1), two(2);
+  a.push_back(std::move(one));
+  a.push_back(std::move(two));
+  assert(size(a) == 2);
+  assert(a[1] == T(2));
+  assert(a[0] == T(1));
+  return true;
+}
+
+TEMPLATE_TEST_CASE("Pushing r-value references", "[cvector][push]", int, Foo)
+{
+  static_assert(push_rref<TestType>());
+  push_rref<TestType>();
+}
+
+template <typename T>
+constexpr bool copy_ctor() {
+  cvector<T, 3> a = { std::in_place, 1, 2 };
+  cvector<T, 3> b = a;
+  assert(size(b) == size(a));
+  assert(b[1] == T(2));
+  assert(b[0] == T(1));
+  assert(a[1] == T(2));
+  assert(a[0] == T(1));
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Copy contructor", "[cvector][ctor]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a = { std::in_place, 1, 2 };
-    cvector<TestType, 3> b = a;
-    assert(size(b) == size(a));
-    assert(b[1] == TestType(2));
-    assert(b[0] == TestType(1));
-    assert(a[1] == TestType(2));
-    assert(a[0] == TestType(1));
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(copy_ctor<TestType>());
+  copy_ctor<TestType>();
 }
 
+template <typename T>
+constexpr bool move_ctor() {
+  cvector<T, 3> a = { std::in_place, 1, 2 };
+  cvector<T, 3> b = std::move(a);
+  assert(size(b) == 2);
+  assert(b[1] == T(2));
+  assert(b[0] == T(1));
+  return true;
+  }
+
 TEMPLATE_TEST_CASE("Move contructor", "[cvector][ctor]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a = { std::in_place, 1, 2 };
-    cvector<TestType, 3> b = std::move(a);
-    assert(size(b) == 2);
-    assert(b[1] == TestType(2));
-    assert(b[0] == TestType(1));
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(move_ctor<TestType>());
+  move_ctor<TestType>();
+}
+
+template <typename T>
+constexpr bool copy() {
+  cvector<T, 3> a = { std::in_place, 1, 2 };
+  cvector<T, 3> b;
+  b = a;
+  assert(size(b) == size(a));
+  assert(b[1] == T(2));
+  assert(b[0] == T(1));
+  assert(a[1] == T(2));
+  assert(a[0] == T(1));
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Copy assignment", "[cvector][assign]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a = { std::in_place, 1, 2 };
-    cvector<TestType, 3> b;
-    b = a;
-    assert(size(b) == size(a));
-    assert(b[1] == TestType(2));
-    assert(b[0] == TestType(1));
-    assert(a[1] == TestType(2));
-    assert(a[0] == TestType(1));
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(copy<TestType>());
+  copy<TestType>();
+}
+
+template <typename T>
+constexpr bool copy_smaller() {
+  cvector<T, 3> a = { std::in_place, 1, 2, 3 };
+  cvector<T, 3> b = { std::in_place, 4, 5 };
+  b = a;
+  assert(size(b) == size(a));
+  assert(b[2] == T(3));
+  assert(b[1] == T(2));
+  assert(b[0] == T(1));
+  assert(a[2] == T(3));
+  assert(a[1] == T(2));
+  assert(a[0] == T(1));
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Copy assignment smaller to larger", "[cvector][assign]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a = { std::in_place, 1, 2, 3 };
-    cvector<TestType, 3> b = { std::in_place, 4, 5 };
-    b = a;
-    assert(size(b) == size(a));
-    assert(b[2] == TestType(3));
-    assert(b[1] == TestType(2));
-    assert(b[0] == TestType(1));
-    assert(a[2] == TestType(3));
-    assert(a[1] == TestType(2));
-    assert(a[0] == TestType(1));
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(copy_smaller<TestType>());
+  copy_smaller<TestType>();
+}
+
+template <typename T>
+constexpr bool copy_larger() {
+  cvector<T, 3> a = { std::in_place, 1, 2 };
+  cvector<T, 3> b = { std::in_place, 3, 4, 5 };
+  b = a;
+  assert(size(b) == size(a));
+  assert(b[1] == T(2));
+  assert(b[0] == T(1));
+  assert(a[1] == T(2));
+  assert(a[0] == T(1));
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Copy assignment larger to smaller", "[cvector][assign]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a = { std::in_place, 1, 2 };
-    cvector<TestType, 3> b = { std::in_place, 3, 4, 5 };
-    b = a;
-    assert(size(b) == size(a));
-    assert(b[1] == TestType(2));
-    assert(b[0] == TestType(1));
-    assert(a[1] == TestType(2));
-    assert(a[0] == TestType(1));
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(copy_larger<TestType>());
+  copy_larger<TestType>();
+}
+
+template <typename T>
+constexpr bool move() {
+  cvector<T, 3> a = { std::in_place, 1, 2 };
+  cvector<T, 3> b;
+  b = std::move(a);
+  assert(size(b) == 2);
+  assert(b[1] == T(2));
+  assert(b[0] == T(1));
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Move assignment", "[cvector][assign]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a = { std::in_place, 1, 2 };
-    cvector<TestType, 3> b;
-    b = std::move(a);
-    assert(size(b) == 2);
-    assert(b[1] == TestType(2));
-    assert(b[0] == TestType(1));
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(move<TestType>());
+  move<TestType>();
+}
+
+template <typename T>
+constexpr bool move_smaller() {
+  cvector<T, 3> a = { std::in_place, 1, 2, 3 };
+  cvector<T, 3> b = { std::in_place, 4, 5 };
+  b = std::move(a);
+  assert(size(b) == 3);
+  assert(b[2] == T(3));
+  assert(b[1] == T(2));
+  assert(b[0] == T(1));
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Move assignment larger to smaller", "[cvector][assign]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a = { std::in_place, 1, 2, 3 };
-    cvector<TestType, 3> b = { std::in_place, 4, 5 };
-    b = std::move(a);
-    assert(size(b) == 3);
-    assert(b[2] == TestType(3));
-    assert(b[1] == TestType(2));
-    assert(b[0] == TestType(1));
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(move_smaller<TestType>());
+  move_smaller<TestType>();
+}
+
+template <typename T>
+constexpr bool move_larger() {
+  cvector<T, 3> a = { std::in_place, 1, 2 };
+  cvector<T, 3> b = { std::in_place, 3, 4, 5 };
+  b = std::move(a);
+  assert(size(b) == 2);
+  assert(b[1] == T(2));
+  assert(b[0] == T(1));
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Move assignment smaller to larger", "[cvector][assign]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a = { std::in_place, 1, 2 };
-    cvector<TestType, 3> b = { std::in_place, 3, 4, 5 };
-    b = std::move(a);
-    assert(size(b) == 2);
-    assert(b[1] == TestType(2));
-    assert(b[0] == TestType(1));
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(move_larger<TestType>());
+  move_larger<TestType>();
+}
+
+template <typename T>
+constexpr bool clear() {
+  cvector<T, 3> a = { std::in_place, 1, 2, 3 };
+  assert(size(a) == 3);
+  a.clear();
+  assert(size(a) == 0);
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Clearing", "[cvector][clear]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 3> a = { std::in_place, 1, 2, 3 };
-    assert(size(a) == 3);
-    a.clear();
-    assert(size(a) == 0);
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(clear<TestType>());
+  clear<TestType>();
+}
+
+template <typename T>
+constexpr bool iteration() {
+  cvector<T, 16> a = { std::in_place, 1, 2, 3, 4 };
+  assert(size(a) == 4);
+  assert(a.begin() == a.begin());
+  assert(a.end() == a.end());
+  assert(a.end() - a.begin() == size(a));
+  assert(*(a.begin() + 1) == a[1]);
+  assert(a.begin()[1] == a[1]);
+  assert(*(a.end() - 1) == a[3]);
+  assert(a.end()[-1] == a[3]);
+  return true;
 }
 
 TEMPLATE_TEST_CASE("Iterators", "[cvector][iteration]", int, Foo) {
-  constexpr auto test = [] {
-    cvector<TestType, 16> a = { std::in_place, 1, 2, 3, 4 };
-    assert(size(a) == 4);
-    assert(a.begin() == a.begin());
-    assert(a.end() == a.end());
-    assert(a.end() - a.begin() == size(a));
-    assert(*(a.begin() + 1) == a[1]);
-    assert(a.begin()[1] == a[1]);
-    assert(*(a.end() - 1) == a[3]);
-    assert(a.end()[-1] == a[3]);
-    return true;
-  };
-  static_assert(test());
-  test();
+  static_assert(iteration<TestType>());
+  iteration<TestType>();
 }
 
 TEST_CASE("Iteration", "[cvector][iteration]") {
@@ -457,19 +504,25 @@ TEST_CASE("Iteration", "[cvector][iteration]") {
   assert(total == 10);
 }
 
-TEST_CASE("Test with references", "[cvector]") {
-  constexpr auto test = [] {
-    int i = 1;
-    cvector<std::reference_wrapper<int>, 5> a = { std::in_place, std::ref(i), std::ref(i), std::ref(i) };
-    assert(size(a) == 3);
-    assert(a.pop_back() == 1 && i == 1);
-    assert(a.pop_back() == 1 && i == 1);
-    a.back().get() = 2;
-    a.push_back(std::ref(i));
-    assert(a.pop_back() == 2);
-    assert(a.pop_back() == 2);
-    return true;
+constexpr bool references() {
+  int i = 1;
+  cvector<std::reference_wrapper<int>, 5> a = {
+    std::in_place,
+    std::ref(i),
+    std::ref(i),
+    std::ref(i)
   };
-  static_assert(test());
-  test();
+  assert(size(a) == 3);
+  assert(a.pop_back() == 1 && i == 1);
+  assert(a.pop_back() == 1 && i == 1);
+  a.back().get() = 2;
+  a.push_back(std::ref(i));
+  assert(a.pop_back() == 2);
+  assert(a.pop_back() == 2);
+  return true;
+}
+
+TEST_CASE("Test with references", "[cvector]") {
+  static_assert(references());
+  references();
 }
